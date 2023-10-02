5c125af5e5dbd175c1e664ef02ce56863355a522
Geetali Matta <gm7761@srmist.edu.in>
2023-10-02 18:51:24 +0530

    Updated the Testimonials page

---
+++ src/components/testimonials.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "../components/testimonials.css";
+
+
+function Index() {
+  return (
+    <>
+      <div className="title">
+        <h1>TESTIMONIALS</h1>
+      </div>
+
+      <div className="box">
+        <div className="box1">
+          <img
+            src="https://images.unsplash.com/photo-1503965830912-6d7b07921cd1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+            alt="image"
+          />
+
+          <p className='paragraph'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quod perspiciatis eos, soluta aspernatur voluptatibus labore quibusdam voluptate iusto. Quisquam dolorum beatae ea blanditiis mollitia ex necessitatibus nisi placeat consequuntur?</p>
+        </div>
+
+        <div className="box2">
+          <img
+            src="https://images.unsplash.com/photo-1503965830912-6d7b07921cd1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+            alt="image"
+          />
+
+          <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur optio nulla cupiditate mollitia! Debitis similique at obcaecati libero? Possimus, fuga provident at eaque culpa vero consequuntur exercitationem ex distinctio doloribus.</p>
+        </div>
+
+        <div className="box1">
+          <img
+            src="https://images.unsplash.com/photo-1503965830912-6d7b07921cd1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+            alt="image"
+          />
+
+          <p className='paragraph'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam repellat quae culpa ad repellendus minus amet quis excepturi obcaecati, natus nam maiores vel incidunt. Minima totam illo repudiandae ducimus iste!</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Index
