aee49a21ede89c891d8f92c63443d407e251b72a
Harish Sekar <113569972+harishrsekar@users.noreply.github.com>
2023-10-03 20:02:21 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Events from './components/events';
 import Landing_page from './components/landing-page';
 import Index from './components/testimonials';
 import logo from './assets/logo.svg';
+import Gallery from "./components/gallery";
 function App() {
   return (
     <div className="App">
